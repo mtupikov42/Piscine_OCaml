d429afa6c78779c69bba94deee75a82375239b67
Unknown <alkomoon2012@gmail.com>
2019-10-07 22:03:47 +0300

    d05 initial

---
+++ d05/ex00/micronap.ml
@@ -0,0 +1,22 @@
+let my_sleep () =
+	Unix.sleep 1
+
+let micronap seconds =
+	for i = 0 to seconds do
+		my_sleep ()
+	done
+
+let main () =
+	if (Array.length Sys.argv) = 2 then
+		let arg = Sys.argv 1 in
+		let seconds =
+			try
+				int_of_string arg
+			with
+			| _ -> 0
+		in
+		micronap seconds
+	else
+		()
+
+let () = main ()
